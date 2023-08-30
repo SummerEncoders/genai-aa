e30fe06b7f1ba9a7d1d9812e5245eef9d6255809
Ivan Molto <ivanmolto@gmail.com>
2023-08-30 08:38:29 +0200

    chore: Configure Providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,83 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import merge from "lodash.merge";
+import type { AppProps } from "next/app";
+import {
+  connectorsForWallets,
+  getDefaultWallets,
+  lightTheme,
+  RainbowKitProvider,
+  Theme,
+} from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { arbitrumGoerli } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import Layout from "../components/layout";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [arbitrumGoerli],
+  [publicProvider()],
+);
+
+const myTheme = merge(lightTheme(), {
+  colors: {
+    accentColor: "#4f46e5",
+  },
+} as Theme);
+
+const projectId = "79e68291741ebb0e75f64ca1ecaf5770";
+
+const { wallets } = getDefaultWallets({
+  appName: "GenNFTs",
+  projectId,
+  chains,
+});
+
+const customAppInfo = {
+  appName: "GenNFTs",
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "Other",
+    wallets: [
+      argentWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+      ledgerWallet({ projectId, chains }),
+    ],
+  },
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        theme={myTheme}
+        appInfo={customAppInfo}
+        chains={chains}
+      >
+        <Layout>
+          <div className={inter.className}>
+            <Component {...pageProps} />
+          </div>
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
