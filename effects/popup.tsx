d2aac8bc8c0feb12004854dc12a045ce468f0b7c
Ivan Molto <ivanmolto@gmail.com>
2023-08-30 08:37:25 +0200

    chore: Add effects

---
+++ effects/popup.tsx
@@ -0,0 +1,11 @@
+export default function Popup() {
+  return (
+    <div className="toast toast-end">
+      <div className="alert alert-success">
+        <div className="flex flex-row">
+          <span>Relay Request Executed Successfully</span>
+        </div>
+      </div>
+    </div>
+  );
+}
